cc4ff1a5a5d9c21236ac2a5c46dd30cf6c2ddd5e
Michael McKay <mamckay89@gmail.com>
2018-11-05 13:44:19 -0700

    onward to victory!

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,5 +1,4 @@
 'use strict';
-/* global store, cuid */
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
@@ -57,7 +56,13 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+    Item.validateName(itemName);
+    Item.create.push({ id: cuid(), name: itemName, checked: false }); 
+    render();     
+    } catch (error) {
+      throw new Error('Cannot add Item: {%s}', error);
+    }
   }
   
   function handleNewItemSubmit() {
--- scripts/store.js
+++ scripts/store.js
@@ -4,14 +4,55 @@ const store = (function() {
   
   const items = [];
   let hideCheckedItems = false;
-  
- 
   let searchTerm = '';
+
+  const addItem = function (name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  
+  const findAndToggleChecked = function (id) {
+    this.findById(id).checked = !this.findById(id).checked;
+  }
+
+  const findAndUpdateName = function (id, newName) {
+    try {
+      Item.validateName(newName);
+      this.findById(id).name = newName;
+    } catch (error) {
+      throw new Error(error); 
+    }
+  }
+
+  const findId = function (item) {
+    return $(item)
+      .closest('.js-item-element')
+      .data('item-id');
+  }
+
+  const findById = function(id){
+    return store.items.find(id);
+  };
+
+  const findAndDelete = function (id) {
+    this.items.splice(this.items.findIndex(ids => ids === id),1); 
+   }
+
   
   return { 
     items, 
     hideCheckedItems,
     searchTerm,
+    addItem,
+    findAndDelete,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findById,
+    findId
   };
   
 }());
